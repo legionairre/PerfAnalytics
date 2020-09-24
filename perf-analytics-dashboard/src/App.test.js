67b105ab4ee6fd44915782bfe7cbb5729f64d9d3
legionairre <guclu.kivanc@gmail.com>
2020-09-24 21:08:26 +0300

    Unit tests added

--- perf-analytics-dashboard/src/App.test.js
+++ perf-analytics-dashboard/src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import Dashboard from "./Dashboard/Dashboard";
+
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+  });
+
+  it('should render just one Dashboard component', () => {
+    expect(wrapper.find(Dashboard)).toHaveLength(1);
+  });
 });
--- perf-analytics-dashboard/src/LineChart/LineChart.js
+++ perf-analytics-dashboard/src/LineChart/LineChart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 
 const LineChart = (props) => {
