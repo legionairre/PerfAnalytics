eed089a83a1049f734d0268cf2e3974b290620da
legionairre <guclu.kivanc@gmail.com>
2020-09-24 16:39:54 +0300

    New test scenario has been added.

--- perf-analytics-api/tests/unit/server.spec.js
+++ perf-analytics-api/tests/unit/server.spec.js
@@ -17,23 +17,50 @@ describe('API Endpoint Controls', () => {
   });
 
   it('should run / POST method with body', async () => {
-    const res = await request(app).post('/').send([{
+    const res = await request(app).post('/').send([ {
       url: 'http://localhost:3002/',
       analyticType: 'navigation',
       time: '370.11',
       startTime: '0.00',
-      createdAt: 1600898041652,
-    }])
+    } ])
     expect(res.statusCode).toEqual(200);
   });
 
+  it('should run / POST method with body and / GET posted data', async () => {
+
+    let expectedDocument = false;
+
+    await request(app).post('/').send([ {
+      url: 'http://localhost:3002/',
+      analyticType: 'navigation',
+      time: '370.11',
+      startTime: '0.00',
+    } ])
+
+    const getResponse = await request(app).get('/').query({
+      beginTime: new Date(new Date().getTime() - (30 * 60 * 1000)).getTime(),
+      endTime: new Date().getTime()
+    });
+
+    getResponse.body.forEach(doc => {
+      if (
+        doc.url === "http://localhost:3002/" &&
+        doc.analyticType === "navigation" &&
+        doc.time === "370.11" &&
+        doc.startTime === "0.00"
+      ) expectedDocument = true;
+    });
+
+    expect(expectedDocument).toEqual(true);
+  });
+
   it('should run / POST method with invalid body', async () => {
-    const res = await request(app).post('/').send([{
+    const res = await request(app).post('/').send([ {
       analyticType: 'navigation',
       time: '370.11',
       startTime: '0.00',
       createdAt: 1600898041652,
-    }])
+    } ])
     expect(res.statusCode).toEqual(500);
   });
 
